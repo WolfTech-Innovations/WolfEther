0e9509cbe7705c96e2a57b15b1a56abbe7dff298
WolfTech Innovations <SpoinkOSGithub@gmail.com>
2024-12-11 21:37:35 +0000

    Fix all

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,141 +1,487 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"bytes"
+	"context"
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"sort"
+	"sync"
+	"time"
 
-    "gasless-blockchain/pkg/blockchain"
-    "gasless-blockchain/pkg/consensus"
-    "gasless-blockchain/pkg/network"
-    "gasless-blockchain/pkg/token"
-    "gasless-blockchain/pkg/crypto"
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/ethereum/go-ethereum/p2p/discover"
+	"github.com/ethereum/go-ethereum/rlp"
+
+	"github.com/libp2p/go-libp2p"
+	libp2pcore "github.com/libp2p/go-libp2p/core"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	"github.com/multiformats/go-multiaddr"
 )
 
-type BlockchainNode struct {
-    Blockchain     *blockchain.Blockchain
-    ConsensusEngine *consensus.DelegatedPoS
-    NetworkManager  *network.NetworkManager
-    TokenManager   *token.TokenManager
-    NodeKey        *crypto.NodeKey
+const (
+	// Network Constants
+	WolfEtherVersion = "1.0.0"
+	NetworkID        = 1337
+	DefaultPort      = 30303
+
+	// Consensus Parameters
+	BlockReward         = 50       // Native currency reward for mining
+	DifficultyTarget    = 4         // Leading zero bytes for PoW
+	BlockTime           = 15        // Target block time in seconds
+	MaxBlockTransactions = 100      // Maximum transactions per block
+	BlockchainDBPath    = "./blockchain_data"
+
+	// Protocol Identifiers
+	TransactionProtocol = "/wolfether/tx/1.0.0"
+	BlockProtocol       = "/wolfether/block/1.0.0"
+	SyncProtocol        = "/wolfether/sync/1.0.0"
+)
+
+// Cryptographic Primitives and Utility Structs
+type CryptoUtils struct {
+	keyStore *keystore.KeyStore
 }
 
-func NewBlockchainNode() *BlockchainNode {
-    // Generate node cryptographic keys
-    nodeKey, err := crypto.GenerateNodeKey()
-    if err != nil {
-        log.Fatalf("Failed to generate node key: %v", err)
-    }
-
-    // Initialize blockchain
-    bc := blockchain.NewBlockchain()
-
-    // Setup consensus mechanism
-    consensusEngine := &consensus.DelegatedPoS{
-        Validators:          []*consensus.Validator{},
-        EpochDuration:       time.Hour,
-        MinStakeRequirement: big.NewInt(10000),
-    }
-
-    // Initialize network manager
-    networkManager := network.NewNetworkManager()
-
-    // Create token manager
-    tokenManager, err := token.NewTokenManager()
-    if err != nil {
-        log.Fatalf("Failed to create token manager: %v", err)
-    }
-
-    return &BlockchainNode{
-        Blockchain:     bc,
-        ConsensusEngine: consensusEngine,
-        NetworkManager:  networkManager,
-        TokenManager:   tokenManager,
-        NodeKey:        nodeKey,
-    }
+type NetworkAddress struct {
+	IP   net.IP
+	Port uint16
 }
 
-func (node *BlockchainNode) Start() error {
-    // Start network discovery
-    if err := node.NetworkManager.StartDiscovery(); err != nil {
-        return fmt.Errorf("network discovery failed: %v", err)
-    }
-
-    // Initialize validators
-    if err := node.initializeValidators(); err != nil {
-        return fmt.Errorf("validator initialization failed: %v", err)
-    }
-
-    // Start consensus mechanism
-    go node.runConsensusLoop()
-
-    // Start blockchain synchronization
-    go node.synchronizeBlockchain()
-
-    return nil
+// Comprehensive Block Structure
+type Block struct {
+	Header       BlockHeader
+	Transactions []*Transaction
+	Signature    []byte
+	Hash         []byte
+	Nonce        uint64
 }
 
-func (node *BlockchainNode) initializeValidators() error {
-    // Add initial validators
-    initialValidators := []*consensus.Validator{
-        {
-            Address:        node.NodeKey.PublicAddress(),
-            StakedTokens:   big.NewInt(100000),
-            LastValidated:  time.Now().Unix(),
-            Reputation:     1.0,
-        },
-    }
-
-    for _, validator := range initialValidators {
-        node.ConsensusEngine.Validators = append(
-            node.ConsensusEngine.Validators, 
-            validator,
-        )
-    }
-
-    return nil
+// Enhanced Block Header
+type BlockHeader struct {
+	Version         uint32
+	PreviousHash    []byte
+	MerkleRoot      []byte
+	Timestamp       uint64
+	Height          uint64
+	DifficultyTarget uint32
+	Coinbase        common.Address
 }
 
-func (node *BlockchainNode) runConsensusLoop() {
-    for {
-        // Select validators for the current epoch
-        selectedValidators := node.ConsensusEngine.SelectValidators()
-
-        // Create and propose new blocks
-        for _, validator := range selectedValidators {
-            if node.ConsensusEngine.ValidateBlock(validator) {
-                // Prepare transactions
-                transactions := node.prepareTransactions()
-
-                // Add block to blockchain
-                node.Blockchain.AddBlock(transactions, validator.Address)
-            }
-        }
-
-        // Sleep until next epoch
-        time.Sleep(node.ConsensusEngine.EpochDuration)
-    }
+// Advanced Transaction Structure
+type Transaction struct {
+	From        common.Address
+	To          common.Address
+	Value       *big.Int
+	Data        []byte
+	Nonce       uint64
+	Signature   []byte
+	GasLimit    uint64
+	GasPrice    *big.Int
+	CreatedAt   time.Time
+	ContractTx  bool
 }
 
-func (node *BlockchainNode) prepareTransactions() []*blockchain.Transaction {
-    // Logic to collect and validate pending transactions
-    return []*blockchain.Transaction{}
+// Advanced Account Model
+type Account struct {
+	Address     common.Address
+	PublicKey   []byte
+	Balance     *big.Int
+	Nonce       uint64
+	StorageRoot []byte
+	CodeHash    []byte
 }
 
-func (node *BlockchainNode) synchronizeBlockchain() {
-    for {
-        // Sync with network peers
-        node.NetworkManager.SyncBlocks(node.Blockchain)
-        time.Sleep(5 * time.Minute)
-    }
+// Comprehensive Blockchain State
+type Blockchain struct {
+	chain            []*Block
+	accountState     map[common.Address]*Account
+	transactionPool  []*Transaction
+	stateMutex       sync.RWMutex
+	difficulty       *big.Int
+	networkNodes     map[peer.ID]*NetworkNode
 }
 
+// Advanced Consensus Mechanism
+type ConsensusEngine struct {
+	blockchain      *Blockchain
+	miningScheduler *MiningScheduler
+	validator       *BlockValidator
+}
+
+// Enhanced Mining Scheduler
+type MiningScheduler struct {
+	miningQueue     chan *Block
+	stopMining      chan struct{}
+	currentMiner    common.Address
+	miningInterval  time.Duration
+	miningStrategy  MiningStrategy
+}
+
+// Mining Strategy Interface
+type MiningStrategy interface {
+	SelectTransactions(pool []*Transaction) []*Transaction
+	AdjustDifficulty(blockchain *Blockchain) *big.Int
+}
+
+// Block Validator
+type BlockValidator struct {
+	blockchain *Blockchain
+}
+
+// Advanced Networking Component
+type NetworkNode struct {
+	host             host.Host
+	peerStore        map[peer.ID]peer.AddrInfo
+	bootstrapNodes   []multiaddr.Multiaddr
+	discoveryService *PeerDiscoveryService
+	blockchain       *Blockchain
+	consensusEngine  *ConsensusEngine
+}
+
+// Peer Discovery Service
+type PeerDiscoveryService struct {
+	host           host.Host
+	peerDiscovered chan peer.AddrInfo
+	routingTable   *RoutingTable
+}
+
+// Routing Table for Efficient Peer Management
+type RoutingTable struct {
+	peers      map[peer.ID]peer.AddrInfo
+	peersMutex sync.RWMutex
+}
+
+// Smart Contract Registry
+type SmartContractRegistry struct {
+	contracts map[common.Address]*SmartContract
+	mutex     sync.RWMutex
+}
+
+type SmartContract struct {
+	Address     common.Address
+	Code        []byte
+	Storage     map[string][]byte
+	Owner       common.Address
+	Executable  func(tx *Transaction) ([]byte, error)
+}
+
+// Cryptographic Utility Methods
+func (cu *CryptoUtils) GenerateKeyPair() (*ecdsa.PrivateKey, common.Address, error) {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		return nil, common.Address{}, err
+	}
+	address := crypto.PubkeyToAddress(privateKey.PublicKey)
+	return privateKey, address, nil
+}
+
+func (cu *CryptoUtils) SignTransaction(tx *Transaction, privateKey *ecdsa.PrivateKey) error {
+	txHash := sha256.Sum256(cu.serializeTransaction(tx))
+	signature, err := crypto.Sign(txHash[:], privateKey)
+	if err != nil {
+		return err
+	}
+	tx.Signature = signature
+	return nil
+}
+
+func (cu *CryptoUtils) serializeTransaction(tx *Transaction) []byte {
+	var serialized []byte
+	serialized = append(serialized, tx.From.Bytes()...)
+	serialized = append(serialized, tx.To.Bytes()...)
+	serialized = append(serialized, tx.Value.Bytes()...)
+	serialized = append(serialized, tx.Data...)
+	serialized = append(serialized, byte(tx.Nonce))
+	return serialized
+}
+
+// Blockchain Methods
+func (bc *Blockchain) AddBlock(block *Block) error {
+	bc.stateMutex.Lock()
+	defer bc.stateMutex.Unlock()
+
+	// Validate block
+	if err := bc.validateBlock(block); err != nil {
+		return err
+	}
+
+	// Update account states
+	for _, tx := range block.Transactions {
+		bc.processTransaction(tx)
+	}
+
+	// Add block to chain
+	bc.chain = append(bc.chain, block)
+	return nil
+}
+
+func (bc *Blockchain) validateBlock(block *Block) error {
+	// Check proof of work
+	if !bc.validateProofOfWork(block) {
+		return errors.New("invalid proof of work")
+	}
+
+	// Check transactions
+	for _, tx := range block.Transactions {
+		if err := bc.validateTransaction(tx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (bc *Blockchain) validateProofOfWork(block *Block) bool {
+	hash := sha256.Sum256(bc.serializeBlock(block))
+	difficulty := block.Header.DifficultyTarget
+
+	for i := uint32(0); i < difficulty; i++ {
+		if hash[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (bc *Blockchain) serializeBlock(block *Block) []byte {
+	var serialized []byte
+	serialized = append(serialized, block.Header.PreviousHash...)
+	serialized = append(serialized, block.Header.MerkleRoot...)
+	serialized = append(serialized, byte(block.Header.Timestamp))
+	serialized = append(serialized, byte(block.Nonce))
+	return serialized
+}
+
+func (bc *Blockchain) processTransaction(tx *Transaction) error {
+	// Update sender account
+	sender := bc.accountState[tx.From]
+	sender.Balance.Sub(sender.Balance, tx.Value)
+	sender.Nonce++
+
+	// Update recipient account
+	recipient := bc.accountState[tx.To]
+	recipient.Balance.Add(recipient.Balance, tx.Value)
+
+	return nil
+}
+
+// Consensus Engine Methods
+func (ce *ConsensusEngine) MineBlock() (*Block, error) {
+	// Select transactions
+	transactions := ce.miningScheduler.miningStrategy.SelectTransactions(
+		ce.blockchain.transactionPool,
+	)
+
+	// Get previous block
+	prevBlock := ce.blockchain.chain[len(ce.blockchain.chain)-1]
+
+	// Create block
+	block := &Block{
+		Header: BlockHeader{
+			Version:          1,
+			PreviousHash:     prevBlock.Hash,
+			Timestamp:        uint64(time.Now().Unix()),
+			Height:           prevBlock.Header.Height + 1,
+			DifficultyTarget: ce.miningScheduler.miningStrategy.AdjustDifficulty(ce.blockchain).Uint32(),
+		},
+		Transactions: transactions,
+	}
+
+	// Perform Proof of Work
+	for nonce := uint64(0); ; nonce++ {
+		block.Nonce = nonce
+		blockHash := sha256.Sum256(ce.blockchain.serializeBlock(block))
+		block.Hash = blockHash[:]
+
+		if ce.validateBlockHash(block.Hash, block.Header.DifficultyTarget) {
+			break
+		}
+	}
+
+	return block, nil
+}
+
+func (ce *ConsensusEngine) validateBlockHash(hash []byte, difficulty uint32) bool {
+	for i := uint32(0); i < difficulty; i++ {
+		if hash[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// Networking Methods
+func (nn *NetworkNode) InitializeNetworking() error {
+	// Create libp2p host
+	host, err := libp2p.New(
+		libp2p.ListenAddrStrings(
+			fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", DefaultPort),
+		),
+	)
+	if err != nil {
+		return err
+	}
+	nn.host = host
+
+	// Setup protocols
+	nn.host.SetStreamHandler(protocol.ID(TransactionProtocol), nn.handleTransaction)
+	nn.host.SetStreamHandler(protocol.ID(BlockProtocol), nn.handleBlock)
+	nn.host.SetStreamHandler(protocol.ID(SyncProtocol), nn.handleBlockchainSync)
+
+	// Initialize peer discovery
+	nn.discoveryService = &PeerDiscoveryService{
+		host:           host,
+		peerDiscovered: make(chan peer.AddrInfo, 100),
+		routingTable:   &RoutingTable{peers: make(map[peer.ID]peer.AddrInfo)},
+	}
+
+	go nn.discoveryService.StartDiscovery()
+
+	return nil
+}
+
+func (nn *NetworkNode) handleTransaction(stream network.Stream) {
+	defer stream.Close()
+
+	var tx Transaction
+	if err := rlp.Decode(stream, &tx); err != nil {
+		log.Printf("Transaction decode error: %v", err)
+		return
+	}
+
+	// Validate and add to transaction pool
+	if err := nn.blockchain.validateTransaction(&tx); err == nil {
+		nn.blockchain.transactionPool = append(nn.blockchain.transactionPool, &tx)
+	}
+}
+
+func (nn *NetworkNode) handleBlock(stream network.Stream) {
+	defer stream.Close()
+
+	var block Block
+	if err := rlp.Decode(stream, &block); err != nil {
+		log.Printf("Block decode error: %v", err)
+		return
+	}
+
+	// Add block to blockchain
+	nn.blockchain.AddBlock(&block)
+}
+
+func (nn *NetworkNode) BroadcastTransaction(tx *Transaction) error {
+	txBytes, err := rlp.EncodeToBytes(tx)
+	if err != nil {
+		return err
+	}
+
+	for _, peerInfo := range nn.peerStore {
+		stream, err := nn.host.NewStream(context.Background(), peerInfo.ID, protocol.ID(TransactionProtocol))
+		if err != nil {
+			continue
+		}
+		stream.Write(txBytes)
+		stream.Close()
+	}
+	return nil
+}
+
+// Peer Discovery Service Methods
+func (pds *PeerDiscoveryService) StartDiscovery() {
+	// Simulated bootstrap nodes
+	bootstrapNodes := []string{
+		"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUqwCgeoLyYyVNEfbcrqBHkT1nk3b",
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPeFqvoE4TWR4TjgFEA6Xc3DRQDNXfzwiWy",
+	}
+
+	for _, nodeAddr := range bootstrapNodes {
+		ma, err := multiaddr.NewMultiaddr(nodeAddr)
+		if err != nil {
+			continue
+		}
+
+		peerInfo, err := peer.AddrInfoFromP2pAddr(ma)
+		if err != nil {
+			continue
+		}
+
+		pds.peerDiscovered <- *peerInfo
+	}
+}
+
+// Smart Contract Registry Methods
+func (scr *SmartContractRegistry) DeployContract(
+	address common.Address, 
+	code []byte, 
+	owner common.Address,
+	executable func(tx *Transaction) ([]byte, error),
+) error {
+	scr.mutex.Lock()
+	defer scr.mutex.Unlock()
+
+	contract := &SmartContract{
+		Address:     address,
+		Code:        code,
+		Owner:       owner,
+		Executable:  executable,
+		Storage:     make(map[string][]byte),
+	}
+
+	scr.contracts[address] = contract
+	return nil
+}
+
+func (scr *SmartContractRegistry) ExecuteContract(tx *Transaction) ([]byte, error) {
+	scr.mutex.RLock()
+	defer scr.mutex.RUnlock()
+
+	contract, exists := scr.contracts[tx.To]
+	if !exists {
+		return nil, errors.New("contract not found")
+	}
+
+	return contract.Executable(tx)
+}
+
+// Main Initialization and Startup
 func main() {
-    node := NewBlockchainNode()
-    
-    if err := node.Start(); err != nil {
-        log.Fatalf("Failed to start blockchain node: %v", err)
-    }
+	// Initialize cryptographic utilities
+	cryptoUtils := &CryptoUtils{
+		keyStore: keystore.NewKeyStore(
+			"./keystore", 
+			keystore.StandardScryptN, 
+			keystore.StandardScryptP,
+		),
+	}
 
-    // Keep node running
-    select {}
-}
+	// Generate node key pair
+	privateKey, nodeAddress, err := cryptoUtils.GenerateKeyPair()
+	if err != nil {
+		log.Fatalf("Key generation error: %v", err)
+	}
+
+	// Initialize blockchain
+	blockchain := &Blockchain{
+		chain:           []*Block{},
+		accountState:    make(map[common.Address]*Account),
+		transactionPool: []*Transaction{},
+		stateMutex:      sync.RWMutex{},
+		difficulty:      &big.Int{},
+		networkNodes:    map[peer.ID]*NetworkNode{},
+	};
